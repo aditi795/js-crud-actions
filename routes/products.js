42c488752160321388203d4b39ce433c3f02d74c
Aditi Desai <adi00373@tjx.com>
2021-05-28 14:59:11 -0400

    Search Product

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const qs = require("qs");
 
 module.exports = function (db) {
   router
@@ -12,25 +13,59 @@ module.exports = function (db) {
       res.send(db.get("products").insert(newProduct).write());
     });
 
-  router.patch("/products/:id", (req, res) => {
-    res.send(
-      db.get("products").find({ id: req.params.id }).assign(req.body).write()
-    );
-  });
+  router.route("/products/search").get((req, res) => {
+    const keywords = req.query.keywords.split(" ");
+    const result = db.get("products").filter((_) => {
+      const fullText = _.description + _.name + _.color;
 
-  router.delete("/products/:id", (req, res) => {
-    db.get("products").remove({ id: req.params.id }).write();
-    res.status(204).send();
-  });
+      return keywords.every((_) => fullText.indexOf(_) !== -1);
+    });
 
-  router.get("/products/:id", (req, res) => {
-  const result = db.get("products").find({ id: req.params.id }).value();
-  if(result){
     res.send(result);
-  }else{
-    res.status(404).send();
-  }
   });
 
+  router.route("/products/detailSearch").get((req, res) => {
+    const query = qs.parse(req.query);
+
+    const results = db.get("products").filter((_) => {
+      return Object.keys(query).reduce((found, key) => {
+        const obj = query[key];
+        switch (obj.op) {
+          case "lt":
+            found = found && _[key] < obj.val;
+            break;
+          case "eq":
+            found = found && _[key] == obj.val;
+            break;
+          default:
+            found = found && _[key].indexOf(obj.val) !== -1;
+            break;
+        }
+        return found;
+      }, true);
+    });
+
+    res.send(results);
+  });
+
+  router
+    .route("/products/:id")
+    .patch((req, res) => {
+      res.send(
+        db.get("products").find({ id: req.params.id }).assign(req.body).write()
+      );
+    })
+    .delete((req, res) => {
+      db.get("products").remove({ id: req.params.id }).write();
+      res.status(204).send();
+    })
+    .get((req, res) => {
+      const result = db.get("products").find({ id: req.params.id }).value();
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(404).send();
+      }
+    });
   return router;
 };
